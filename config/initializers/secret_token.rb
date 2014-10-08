8999e4bf6b79633dcbb0291fb77159336162375c
Isaac Noda <isaacnoda@gmail.com>
2014-10-08 11:16:07 -0500

    rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dwellings::Application.config.secret_key_base = '8aed791f889c75347ed5287cc7a5d785ad9c0171e3c739503a7b2c2739403c8de26d3df53747a49056be2b00f61db90d14732452c791a949524c069a9b4db3c8'
