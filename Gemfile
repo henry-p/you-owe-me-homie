448e351cd7811f7d0032971932e94afa8418b4bf
Isaac Noda <isaacnoda@gmail.com>
2014-10-08 11:39:36 -0500

    added shoulda matchers

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ group :development, :test do
   gem 'capybara', '~> 2.2.1'
   gem 'database_cleaner', '~> 1.2.0'
   gem 'launchy'
+  gem 'shoulda-matchers', require: false
 end
 
 # Use ActiveModel has_secure_password
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require 'rspec/rails'
 require 'rspec/autorun'
 require 'database_cleaner'
 require 'launchy'
+require 'shoulda/matchers'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
