39fda16b9e4b7f45564c02800a6f6fe802f25990
Wil Chandler <wilchandler2@gmail.com>
2014-10-09 16:20:45 -0500

    fixed placement of redirect

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -11,23 +11,25 @@ class TransactionsController < ApplicationController
 
     if !params[:members]
       flash[:error] = "There was an issue creating the bill. Please select at least one member to bill."
-      redirect_to group_path(group)
     else
-        params[:members].each do |key, email|
-          user = User.find_by_email(email)
-          t = Transaction.new(
-            from_user: current_user,
-            to_user: user,
-            group_id: group.id,
-            amount: amount,
-            is_bill: is_bill
-          )
-          if !t.save
-            flash[:error] = "There was an issue creating the bill. Please enter a valid amount."
-            redirect_to group_path(group)
-          end
+      params[:members].each do |key, email|
+
+        user = User.find_by_email(email)
+        t = Transaction.new(
+          from_user: current_user,
+          to_user: user,
+          group_id: group.id,
+          amount: amount,
+          is_bill: is_bill
+        )
+
+        if !t.save
+          flash[:error] = "There was an issue creating the bill. Please enter a valid amount."
         end
       end
     end
+
+    redirect_to group_path(group)
+  end
 
 end
