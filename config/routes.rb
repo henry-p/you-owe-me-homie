d7a2c276dfd9bb56ec27a03c84d127b505f7ac42
Wil Chandler <wilchandler2@gmail.com>
2014-10-09 17:44:48 -0500

    fixed view errors and spec syntax because of adding sti

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,14 @@ Dwellings::Application.routes.draw do
   root "sessions#new"
   post "/login" => "sessions#create", as: "login"
   delete "/logout" => "sessions#destroy", as: "logout"
+  get "/groups/:group_id/bills" => "bills#new", as: "new_group_bill"
+  get "/groups/:group_id/payments" => "payments#new", as: "new_group_payment"
+  post "/groups/:group_id/bills" => "bills#create", as: "group_bills"
+  post "/groups/:group_id/payments" => "payments#create", as: "group_payments"
 
   resources :groups, except: [:index] do
     resources :comments, only: [:new, :create, :show]
-    resources :transactions do
+    resources :transactions, only: [:delete, :show] do
       resources :comments, only: [:new, :create, :show]
     end
   end
